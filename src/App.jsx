7eff226e07f18efc11435fb50e1d0de9cfb1611a
Muhamed Yasin <56083787+Muhamed-Yasin@users.noreply.github.com>
2024-12-31 09:09:35 +0530

    SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
 import { Navigation } from "./components/navigation";
 import { Header } from "./components/header";
 import { Features } from "./components/features";
@@ -25,6 +26,13 @@ const App = () => {
 
   return (
     <div>
+      <Helmet>
+        <title>Sunway Energy Solutions</title>
+        <meta
+          name="description"
+          content="Sunway Energy Solutions is a solar power company based in Kerala, India. We provide solar power solutions for residential and commercial buildings." />
+          <meta name="keywords" content="solar panels, solar energy, solar panel installation, Kerala, solar solutions, sustainable energy, renewable energy, solar power, solar panels in Kerala" />
+      </Helmet>
       <Navigation />
       <Header data={landingPageData.Header} />
       <Features data={landingPageData.Features} />
